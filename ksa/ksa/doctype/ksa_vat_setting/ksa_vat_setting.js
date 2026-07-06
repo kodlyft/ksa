bf98b5b88742179bdc86b4cb1ef13e2d8a8f72c1
Ali Raza <soundsofnation29@gmail.com>
2026-07-06 14:44:15 +0500

    feat: Implement setup function for KSA VAT Setting to configure queries for purchase and sales accounts

--- ksa/ksa/doctype/ksa_vat_setting/ksa_vat_setting.js
+++ ksa/ksa/doctype/ksa_vat_setting/ksa_vat_setting.js
@@ -2,6 +2,28 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("KSA VAT Setting", {
+	setup: function (frm) {
+		const tables = ["ksa_vat_purchase_accounts", "ksa_vat_sales_accounts"];
+
+		const queries = {
+			account: {
+				account_type: "Tax",
+				is_group: 0,
+			},
+			item_tax_template: {
+				disabled: 0,
+			},
+		};
+
+		tables.forEach((table) => {
+			Object.keys(queries).forEach((field) => {
+				frm.set_query(field, table, () => ({
+					filters: queries[field],
+				}));
+			});
+		});
+	},
+
 	onload: function () {
 		frappe.breadcrumbs.add("Accounts", "KSA VAT Setting");
 	},
